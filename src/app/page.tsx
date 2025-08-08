a683112da361d6c7123bbe4bf45f2e919e2e771a
Facundo080808 <facundo_hub@hotmail.com>
2025-08-08 16:00:48 -0300

    Añadir la función fetchCart para cargar el contenido del carrito al iniciar la página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-  
+"use client";
 import BestCombination from "@/components/bestCombinationFilter";
 import ProductList from "@/components/productCardList";
 import { CartButtonLink } from "@/components/UI/cartButtonLink";
+import { useCart } from "@/hooks/useCart";
+import { useEffect } from "react";
 
 
 export default function Home() {
+  const { fetchCart } = useCart();
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-6">Productos</h1>
